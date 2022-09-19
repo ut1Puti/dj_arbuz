f64b30f4987edae30b3b439a9fbee5efc04b7092
ut1Puti <91106828+ut1Puti@users.noreply.github.com>
2022-09-19 09:41:17 +0500

    Initial commit to develop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,7 @@
 import bot.MyBot;
 
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws TelegramApiException, IOException {
-        String filePath = "src/main/resources/application.properties";
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new MyBot(filePath));
+    public static void main(String[] args){
+        System.out.println("Hello world!");
     }
 }
