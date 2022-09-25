6c30bc35a49ca172eb0bf0e1f90c296f59a0f43f
ut1Puti <sololasuklina@gmail.com>
2022-09-26 02:34:42 +0500

    add server

--- src/main/java/handlers/HandlerVkApi.java
+++ src/main/java/handlers/HandlerVkApi.java
@@ -5,17 +5,15 @@ import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
 import com.vk.api.sdk.objects.groups.Group;
 import com.vk.api.sdk.objects.groups.responses.GetByIdObjectLegacyResponse;
+import httpserver.server.HttpServer;
 import user.User;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.*;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,57 +23,31 @@ public class HandlerVkApi {
     private final static TransportClient transportClient = new HttpTransportClient();
     private final static VkApiClient vk = new VkApiClient(transportClient);
 
-    public static User initUser(Scanner input) {
-        //do auto pulling redirect link
-        WebTarget t = ClientBuilder.newClient().target("https://oauth.vk.com/authorize?client_id=51433509&display=page&redirect_uri={redirect_uri}&scope=270336&response_type={type}&v=5.131");
-        t = t.resolveTemplate("redirect_uri", "https://oauth.vk.com/blank.html").resolveTemplate("type", "token");
-        //t = t.resolveTemplate("redirect_uri", "https://t.me/oop_urfu_bot").resolveTemplate("type", "token");
-        // t = t.resolveTemplate("redirect_uri", "http://localhost:8080/redirect.html").resolveTemplate("type", "token");
-        LocalBrowserHandler b = new LocalBrowserHandler();
-        b.openBrowser(t.getUri());
-        //System.out.println(t.request().get());
-        //String l = "http://localhost:8080/redirect.html";
-        //try {
-        //    b.openBrowser(new URI(l));
-        //    URL q = getFinalURL(new URL(l));
-        //    System.out.println(q);
-        //} catch (MalformedURLException | URISyntaxException e) {
-        //    System.out.println(e.getMessage());
-        //}
-        // end auto pulling redirect link
-        //просто копируешь всю ссылку из браузера
-        String link = input.nextLine();
-        int id = getIdFromURL(link);
-        String token = getAccessTokenFromURL(link);
-        return new User(id, token);
-    }
-
-    public static URL getFinalURL(URL url) {
+    public User initUser(Scanner input) {
+        //do auto pulling code
+        HttpServer server;
         try {
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setInstanceFollowRedirects(false);
-            con.setRequestMethod("GET");
-            con.connect();
-            con.getInputStream();
-            int resCode = con.getResponseCode();
-            while(!(resCode == HttpURLConnection.HTTP_SEE_OTHER
-                    || resCode == HttpURLConnection.HTTP_MOVED_PERM
-                    || resCode == HttpURLConnection.HTTP_MOVED_TEMP)){
-                System.out.println(con.getHeaderField("Location"));
-            }
-            if (resCode == HttpURLConnection.HTTP_SEE_OTHER
-                    || resCode == HttpURLConnection.HTTP_MOVED_PERM
-                    || resCode == HttpURLConnection.HTTP_MOVED_TEMP) {
-                String Location = con.getHeaderField("Location");
-                if (Location.startsWith("/")) {
-                    Location = url.getProtocol() + "://" + url.getHost() + Location;
-                }
-                return getFinalURL(new URL(Location));
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            server = HttpServer.getInstance();
+            server.start();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        return url;
+        String url = "https://oauth.vk.com/authorize?client_id=51434490&display=page&redirect_uri=http://localhost:8080/redirect.html&scope=270336&response_type=code&v=5.131";
+        WebTarget t = ClientBuilder.newClient().target(url);
+        System.out.println(t.request().get());
+        // end auto pulling code
+        String code;
+        code = input.nextLine();
+        UserAuthResponse authResponse;
+        try {
+            authResponse = vk.oAuth()
+                    .userAuthorizationCodeFlow(51434490, "dvCZlOdY5gtNvZgvUT8s", "http://localhost:8080/redirect.html", code)
+                    .execute();
+        } catch (ApiException | ClientException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return new User(authResponse.getUserId(), authResponse.getAccessToken());
     }
 
     public static List<Group> searchGroups(String groupName, User user) {
@@ -110,46 +82,11 @@ public class HandlerVkApi {
         return null;
     }
 
-    public static boolean joinGroup(String groupName, User user) {
-        Group verifiedGroup = searchVerifiedGroup(groupName, user);
-        if (verifiedGroup == null) {
-            return false;
-        }
-        if (verifiedGroup.isMember()) {
-            return true;
-        }
-        try {
-            vk.groups().join(user).groupId(verifiedGroup.getId()).execute();
-            return true;
-        } catch (ClientException | ApiException e) {
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
-
     public static void turnNotifications(boolean turn, Group group, User user) {
         try {
             vk.wall().get(user).domain(group.getScreenName()).offset(0).count(3).execute();
         } catch (ClientException | ApiException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    private static int getIdFromURL(String url){
-        int startIndexOfId = url.lastIndexOf("user_id=") + "user_id=".length();
-        StringBuilder id = new StringBuilder("");
-        while (url.charAt(startIndexOfId) != '&' && startIndexOfId < url.length() - 1) {
-            id.append(url.charAt(startIndexOfId++));
-        }
-        return Integer.parseInt(id.toString());
-    }
-
-    private static String getAccessTokenFromURL(String url){
-        int startIndexOfAccessToken = url.lastIndexOf("access_token=") + "access_token=".length();
-        StringBuilder accessToken = new StringBuilder("");
-        while (url.charAt(startIndexOfAccessToken) != '&' && startIndexOfAccessToken < url.length() - 1){
-            accessToken.append(url.charAt(startIndexOfAccessToken++));
-        }
-        return accessToken.toString();
     }
 }
---
+++ src/main/java/httpserver/server/HttpServerConst.java
@@ -0,0 +1,7 @@
+package httpserver.server;
+
+public class HttpServerConst {
+    public static final int PORT = 8080;
+    public static final String WEB_SRC = "src/main/resources/websrc";
+    public static final String CRLF = "\r\n";
+}
