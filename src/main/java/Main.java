17e55287490e164fcdad99442925a7b3df8d06ae
Andrey Shchegolev <andrey@air-andrey.at.urfu.ru>
2022-09-26 11:18:38 +0500

    Add base methods to class Storage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 import bots.ConsoleBot;
 import database.Storage;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-
+import database.Storage;
 public class Main {
     public static void main(String[] args){
+        /*
         Scanner s = new Scanner(System.in);
         Storage storage = new Storage();
         new ConsoleBot().start(s);
         s.close();
+         */
+
     }
 }
--- src/main/java/database/Storage.java
+++ src/main/java/database/Storage.java
@@ -1,8 +1,39 @@
 package database;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class Storage {
-    Map<Integer, String> map = new HashMap();
+    public Map<String, List<String>> map;
+    public void searchUser(String id, String group) {
+        if (map.get(id) == null) {
+            addNewUser(id, group);
+        } else {
+            addInfo2User(id,group);
+        }
+    }
+    public Storage() {
+        map = new HashMap();
+    }
+    private void addNewUser(String id, String group) {
+        List<String> newList = new LinkedList<>();
+        newList.add(group);
+        map.put(id, newList);
+    }
+    private void addInfo2User(String id, String group) {
+        List<String> updatedList = map.get(id);
+        updatedList.add(group);
+        map.put(id,updatedList);
+    }
+
+
+    public static void main(String[] args){
+        Storage temp = new Storage();
+        temp.searchUser("Abc","1");
+        temp.searchUser("Abc","2");
+        System.out.println(temp.map.get("Abc"));
+    }
 }
+
