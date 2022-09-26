42af36de1aaad4b20fc27126bb6b1581b6067ab8
Andrey Shchegolev <andrey@air-andrey.at.urfu.ru>
2022-09-26 13:51:23 +0500

    Class Storage ready

--- src/main/java/database/Storage.java
+++ src/main/java/database/Storage.java
@@ -1,65 +1,106 @@
 package database;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.lang.invoke.SwitchPoint;
+import java.util.*;
 
+/**
+ * Класс для хранения подписок всех пользователей
+ *
+ * @author Щёголев Андрей
+ * @version 0.1
+ */
 public class Storage {
+    /**
+     * Поле хеш таблицы где ключ - айди группы, значение - список пользователей
+     */
     private Map<String, List<String>> map;
 
-    private void addNewUser(String id, String group) {
+    /**
+     * Метод для создания нового пользователя в наш класс
+     *
+     * @param userId  -айди пользователя
+     * @param groupId - айди группы
+     */
+    private void addNewGroup(String groupId, String userId) {
         List<String> newList = new LinkedList<>();
-        newList.add(group);
-        map.put(id, newList);
+        newList.add(userId);
+        map.put(groupId, newList);
     }
 
-    private void addInfoToOldUser(String id, String group) {
-        List<String> updatedList = map.get(id);
-        updatedList.add(group);
-        map.put(id, updatedList);
+    /**
+     * Метод для добавления информации для имеющегося в базе группы
+     *
+     * @param groupId - Айди группы
+     * @param userId
+     */
+    private void addOldGroup(String groupId, String userId) {
+        List<String> updatedList = map.get(groupId);
+        updatedList.add(userId);
+        map.put(groupId, updatedList);
     }
 
-    public void addInfoToUser(String groupID, String userID) {
-        if (map.get(groupID) == null) {
-            addNewUser(groupID, userID);
+    /**
+     * метод для добавления информации  где происходит ветвление на методы добавления старой/новой группы
+     * @param groupId - айди группы
+     * @param userID - айди пользователя
+     * @see Storage#addNewGroup(String, String) 
+     * @see Storage#addOldGroup(String, String)
+     */
+    public void addInfoToGroup(String groupId, String userID) {
+        if (map.get(groupId) == null) {
+            addNewGroup(groupId, userID);
         } else {
-            addInfoToOldUser(groupID, userID);
+            addOldGroup(groupId, userID);
         }
     }
 
+    /**
+     * Конструктор создания класса
+     */
     public Storage() {
-
-        map = new HashMap<String, List<String>>();
+        map = new HashMap<>();
     }
 
-    public List<String> getGroupInformation(String groupId) {
-        return map.get(groupId);
-    }
-
+    /**
+     * Метод для сохранения хеш таблицы в виде файла с расширением json
+     */
     public void saveToJsonFile() {
         Gson gson = new Gson();
         String json = gson.toJson(map);
-        System.out.println(json);
         try {
-            FileOutputStream file = new FileOutputStream("Users_database.json");
-            file.write(json.getBytes());
+            FileWriter file = new FileWriter("src/main/resources/Users_database.json");
+            file.write(json);
+            file.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void main(String[] args) {
-        Storage temp = new Storage();
-        temp.addInfoToUser("Abc", "1");
-        temp.addInfoToUser("Abc1", "2");
-        temp.saveToJsonFile();
+    /**
+     * Метод для возврата хеш таблицы с помощью созданнового json файла
+     * @see Storage#saveToJsonFile()
+     *
+     */
+    public void returnStorageFromDatabase() {
+        try {
+            FileReader file = new FileReader("src/main/resources/Users_database.json");
+            Gson jsonFile = new Gson();
+            Scanner scanner = new Scanner(file);
+            try {
+                String json = scanner.nextLine();
+                map = jsonFile.fromJson(json, new TypeToken<Map<String, List<String>>>() {
+                }.getType());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            file.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
